98d4f4a7741bf6dea359852b24cce9673ee2ae2d
ederson <edersonvillegas50@gmail.com>
2022-06-08 07:17:00 +0800

    add header page identification

--- components/Headers/Header.js
+++ components/Headers/Header.js
@@ -1,10 +1,20 @@
 import React from "react";
-
+import { useRouter } from "next/router";
 export default function Header() {
+  const { pathname } = useRouter();
+
   return (
     <header className="bg-white shadow">
       <div className="px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
-        <h1 className="text-3xl font-bold text-gray-900">Dashboard</h1>
+        <h1 className="text-3xl font-bold text-gray-900">
+          {pathname == "/dashboard"
+            ? "Dashboard"
+            : pathname == "/dashboard/product-categories"
+            ? "Categories"
+            : pathname == "/dashboard/products"
+            ? "Products"
+            : "Point of Sales"}
+        </h1>
       </div>
     </header>
   );
