f7dd440d915db5eb75585f04efd6d6c5ffb8970e
ederson <edersonvillegas50@gmail.com>
2022-06-08 07:06:20 +0800

    show list of product categories

---
+++ components/Tables/Categories.js
@@ -0,0 +1,133 @@
+import React, { useEffect, useState } from "react";
+import { db } from "../../firebase-config";
+import { ref, onValue } from "firebase/database";
+import { PencilAltIcon } from "@heroicons/react/outline";
+export default function Categories() {
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    const categoriesRef = ref(db, "categories/");
+    onValue(categoriesRef, (snapshot) => {
+      const data = snapshot.val();
+      if (data !== categories) setCategories(Object.entries(data).reverse());
+    });
+  }, []);
+
+  const text_truncate = function (str, length, ending) {
+    if (length == null) {
+      length = 100;
+    }
+    if (ending == null) {
+      ending = "...";
+    }
+    if (str.length > length) {
+      return str.substring(0, length - ending.length) + ending;
+    } else {
+      return str;
+    }
+  };
+  return (
+    <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+      <div className="p-4">
+        <label htmlFor="table-search" className="sr-only">
+          Search
+        </label>
+        <div className="relative mt-1">
+          <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+            <svg
+              className="w-5 h-5 text-gray-500 dark:text-gray-400"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+          </div>
+          <input
+            type="text"
+            id="table-search"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-80 pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Search for categories"
+          />
+        </div>
+      </div>
+      <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+          <tr>
+            <th scope="col" className="p-4">
+              <div className="flex items-center">
+                <input
+                  id="checkbox-all-search"
+                  type="checkbox"
+                  className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                />
+                <label htmlFor="checkbox-all-search" className="sr-only">
+                  checkbox
+                </label>
+              </div>
+            </th>
+            <th scope="col" className="px-6 py-3">
+              <span className="sr-only">Edit</span>
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Category Name
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Image
+            </th>
+            <th scope="col">Description</th>
+          </tr>
+        </thead>
+        <tbody>
+          {categories.map((category) => (
+            <tr
+              className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+              key={category[0]}
+            >
+              <td className="w-4 p-4">
+                <div className="flex items-center">
+                  <input
+                    id="checkbox-table-search-1"
+                    type="checkbox"
+                    className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                  />
+                  <label htmlFor="checkbox-table-search-1" className="sr-only">
+                    checkbox
+                  </label>
+                </div>
+              </td>
+              <td className="px-6 py-4 text-right">
+                <button
+                  type="button"
+                  class="text-white bg-[#FF9119] hover:bg-[#FF9119]/80 focus:ring-4 focus:outline-none focus:ring-[#FF9119]/50 font-medium rounded-full text-sm px-5 py-2.5 text-center inline-flex items-center dark:hover:bg-[#FF9119]/80 dark:focus:ring-[#FF9119]/40 mr-2 mb-2"
+                >
+                  <PencilAltIcon height={15} />
+                </button>
+              </td>
+              <th
+                scope="row"
+                className="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap"
+              >
+                {category[1].name}
+              </th>
+              <td className="px-6 py-4">
+                {category[1].image && (
+                  <img
+                    className="object-contain h-48 w-96 ..."
+                    src={category[1].image}
+                  />
+                )}
+              </td>
+              <td className="px-6 py-4">
+                {text_truncate(category[1].description, 50)}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
--- pages/dashboard/product-categories.js
+++ pages/dashboard/product-categories.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Layout from "../../layouts/Dashboard";
 import AddProductCategories from "../../components/Modals/AddProductCategories";
+import ProductCategoriesTable from "../../components/Tables/Categories";
 export default function Index() {
   return (
     <Layout>
       <AddProductCategories />
+      <br />
+      <br />
+      <br />
+      <ProductCategoriesTable />
     </Layout>
   );
 }
